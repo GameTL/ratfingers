0a905515618be6099a645b6b6ce3ce7bd8e0a02b
Game Limsila <limsila.limsila@yahoo.com>
2023-01-20 12:33:32 +0700

    added updirectory function

--- ratfin/__init__.py
+++ ratfin/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '2.1.5'
+__version__ = '2.2.0'
 """ 
 Date: 19 Jan 2023
 """
@@ -24,8 +24,13 @@ def clearterm():
     system = platform.system()
     if system == 'Windows': # for Windows
         os.system("cls")
-    else: # for Unix (MacOS, Linux)
+    elif system == 'Darwin': # for MacOS
         os.system("clear")
+    elif system == 'Linux': # for Linux
+        os.system("clear")
+    else: # for other platforms
+        print('Unknown platform')
+
 
 def printclr(text="enter text", color : str = ''):
     if (color == '') or (color not in colors):
@@ -42,5 +47,31 @@ def printclr(text="enter text", color : str = ''):
     else:
         print("\033[1;{}m{}\033[0m".format(colors[color], text))
 
+def upDirPath(path="", up=1, filename=""):
+    if path == "":
+        printclr("missing argument for upDirPath()","red")
+        printclr("""enter the following on top of the upDirPath() function:
+    
+    path = os.path.dirname(os.path.realpath(__file__))
 
+Your code should look like this:
 
+    path = os.path.dirname(os.path.realpath(__file__))
+    print(upDirPath(path, up=1, filename="nlp_config.json"))
+
+ ""","blue")
+        return "upDirPath() error"
+    else:
+        if platform.system() == 'Windows':
+            path = path.split("\\")
+            # printclr(path,"red")
+            path = "\\".join(path[:-up:]) + f"\\{filename}"
+            # printclr(path,"red")
+            return path
+            
+        else:
+            path = path.split("/")
+            # printclr(path,"red")
+            path = "/".join(path[:-up:]) + f"/{filename}"
+            # printclr(path,"red")
+            return path
